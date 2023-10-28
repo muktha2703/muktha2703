931810b4c85d0e9bbb7c71ca5f45544e6d458217
Muktha S <135826884+muktha2703@users.noreply.github.com>
2023-10-28 12:12:58 +0530

    added vercel.json and build_files.sh

--- digipay/urls.py
+++ digipay/urls.py
@@ -25,3 +25,5 @@ urlpatterns = [
     path("transfer/",include('transactions.urls')),
     path('admin/', admin.site.urls),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
